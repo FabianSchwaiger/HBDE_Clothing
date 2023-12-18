d7d5371d3363a00a1bf16b02d45bee2da14fd300
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2023-12-18 11:10:03 +0100

    formatting

--- src/Main.java
+++ src/Main.java
@@ -16,14 +16,14 @@ public class Main
         String pathOutBeforePoseStripes = "img/stripes/pose";
         String pathMale = "/male/subject_mesh_";
         String pathFemale = "/female/subject_mesh_";
-    
+        
         new File("img/source").mkdirs();
-    
+        
         new File("img/shirt_pants/pose0/male").mkdirs();
         new File("img/shirt_pants/pose1/male").mkdirs();
         new File("img/shirt_pants/pose0/female").mkdirs();
         new File("img/shirt_pants/pose1/female").mkdirs();
-    
+        
         new File("img/stripes/pose0/male").mkdirs();
         new File("img/stripes/pose1/male").mkdirs();
         new File("img/stripes/pose0/female").mkdirs();
@@ -38,7 +38,7 @@ public class Main
             clothier.putOnShirtPants(new File(pathInBeforePose + "1" + pathMale + ending), pathOutBeforePoseShirtPants + "1" + pathMale + ending);
             clothier.putOnShirtPants(new File(pathInBeforePose + "0" + pathFemale + ending), pathOutBeforePoseShirtPants + "0" + pathFemale + ending);
             clothier.putOnShirtPants(new File(pathInBeforePose + "1" + pathFemale + ending), pathOutBeforePoseShirtPants + "1" + pathFemale + ending);
-    
+            
             clothier.putOnStripes(new File(pathInBeforePose + "0" + pathMale + ending), pathOutBeforePoseStripes + "0" + pathMale + ending, 5, 15);
             clothier.putOnStripes(new File(pathInBeforePose + "1" + pathMale + ending), pathOutBeforePoseStripes + "1" + pathMale + ending, 5, 15);
             clothier.putOnStripes(new File(pathInBeforePose + "0" + pathFemale + ending), pathOutBeforePoseStripes + "0" + pathFemale + ending, 5, 15);
